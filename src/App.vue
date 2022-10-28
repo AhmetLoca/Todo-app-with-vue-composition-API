0663894e341e2d59118eff5cc0f96216f4620187
ahmet <locaahmet@gmail.com>
2022-10-28 15:36:39 +0300

    create initial folders

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/todo1.png">
+    <h3>Todo App / Vue3 Composition API</h3>
+    <form>
+      <input type="text">
+      <button>Add</button>
+    </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    return{
+
+    }
   }
 }
 </script>
@@ -21,6 +24,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+img{
+  height:300px;
 }
 </style>
